f9391c0902068b534993f7250aa4301c1e0b9a41
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-04-18 16:36:17 +0300

    feat(custom): added posts list component

---
+++ src/components/PostsList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="list"></div>
+</template>
+
+<script setup>
+  import posts from "../../mocked-posts.json";
+</script>
+
+<style lang="scss" scoped>
+  .list {
+    padding: 0 0 20px 0;
+    gap: 10px 20px;
+    grid-template-columns: minmax(320px, 440px);
+    @include grid-centered;
+
+    @media screen and (min-width: 768px) {
+      grid-template-columns: repeat(2, 320px);
+    }
+
+    @media screen and (min-width: 1024px) {
+      grid-template-columns: repeat(2, 440px);
+    }
+  }
+</style>
